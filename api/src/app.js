ac892c4ddd2e4b41727c681d4a31ba1cd5f1464f
Diego Pahua Silvan <dpahuas@gmail.com>
2022-04-04 11:50:25 -0500

    delete lines and add cors

--- api/src/app.js
+++ api/src/app.js
@@ -7,17 +7,11 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+server.use(express.urlencoded())
 server.use(express.json())
 
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+server.use(cors())
 
 server.use('/', routes);
 
